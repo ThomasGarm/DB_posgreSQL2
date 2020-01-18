6f616709c980984f3183d9bd45ce655db5a59a6f
ThomasGarm <56396464+ThomasGarm@users.noreply.github.com>
2020-01-18 18:53:59 +0100

    Update userview.py

--- view/userview.py
+++ view/userview.py
@@ -16,3 +16,17 @@ class Userview():
         age = input("Entrez votre age")
         password = input("Entrez votre password")
         self.usermodel.create_an_account(name, firstname, pseudo, email, age, password)
+        
+    def delete_account(self):
+        
+         pseudo = input("Entrez votre pseudo  ")
+         self.usermodel.delete_account(pseudo)
+        
+    def to_update_account(self):
+        pseudo = input("Entrez votre pseudo  ")
+        column= input("Que voulez-vous modifier ? 'name 'lastname' 'pseudo' 'email' 'age' 'password')
+        value = input(f"Choisissez votre nouveau {'column'})
+        self.usermodel.update_an_account()
+        
+        
+        
